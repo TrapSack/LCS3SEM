de1f38c4fbdb5ae2f77a186346ccd986922189ea
TrapSack <61499327+TrapSack@users.noreply.github.com>
2020-11-29 23:17:16 +0300

    Added Lab3 Staselko Protasov

---
+++ Parser.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace LR2
+{
+    public class Parser
+    {
+        private readonly IParser parser;
+
+        public Parser(string path)
+        {
+            if (Path.GetExtension(path) == ".xml")
+            {
+                parser = new XmlParser(path);
+            }
+            else if (Path.GetExtension(path) == ".json")
+            {
+                parser = new JsonParser(path);
+            }
+        }
+
+        public T Parse<T>() where T : new()
+        {
+            return parser.Parse<T>();
+        }
+    }
+}
